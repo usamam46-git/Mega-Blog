7016d6ac271091169132bdda24a30b2c17f9d068
usamam46-git <usamam46@gmail.com>
2025-10-05 11:55:19 +0500

    Setting up path issue

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '80px'}) {
   return (
-    <div className='rounded-full overflow-hidden'><img src="/mainlogo.jpg" alt="Logo" style={{ width }}  /></div>
+    <div className='rounded-full overflow-hidden'><img src="./mainlogo.jpg" alt="Logo" style={{ width }}  /></div>
   )
 }
 
